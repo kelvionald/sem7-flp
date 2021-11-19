4c305b449c9a85d943a3df4970f8327783b83b15
kelvionald <31734063+kelvionald@users.noreply.github.com>
2021-11-19 16:41:19 +0300

    7 проверка состояний на достижимость

--- lw7.hs
+++ lw7.hs
@@ -1,6 +1,7 @@
 import Data.List
 import Data.Array
 import Debug.Trace
+import Data.Typeable
 
 -- объект
 -- волк-коза-капуста-фермер
@@ -19,18 +20,32 @@ from R = L
 type Position = Array Item Location
 
 -- начальная и целевая позиция
--- a
+
+-- а) фермер перевозит волка, козла и капусту на другой берег.
 -- startPosition = listArray (Wolf, Farmer) [L, L, L, L]
 -- goalPosition = listArray (Wolf, Farmer) [R, R, R, R]
--- b
-startPosition = listArray (Wolf, Farmer) [L, L, L, L]
-goalPosition = listArray (Wolf, Farmer) [R, R, R, L]
+
+-- b) фермер перевозит волка, козла и капусту на другой берег, а сам возвращается.
+-- startPosition = listArray (Wolf, Farmer) [L, L, L, L]
+-- goalPosition = listArray (Wolf, Farmer) [R, R, R, L] -- wrong
+
+-- c) фермер перевозит на другой берег волка и козла, а капуста уже находится на том берегу.
+-- startPosition = listArray (Wolf, Farmer) [L, L, R, L]
+-- goalPosition = listArray (Wolf, Farmer) [R, R, R, R]
+
+-- d) фермер перевозит на другой берег волка и козла, а капусту оставляет.
+-- startPosition = listArray (Wolf, Farmer) [L, L, L, L]
+-- goalPosition = listArray (Wolf, Farmer) [R, R, L, R]
+
+-- e) фермер перевозит на другой берег волка и капусту, а козёл уже ждет на том берегу.
+-- startPosition = listArray (Wolf, Farmer) [L, R, L, L]
+-- goalPosition = listArray (Wolf, Farmer) [R, R, R, R]
 
 -- неправильная позиция: без контроля человека остаются
 -- волк с козлом или козел с капустой
-wrongPosition :: Position -> [Solution] -> Bool
-wrongPosition p sols =
-    all (/= p!Farmer) [p!Wolf, p!Goat] || all (/= p!Farmer) [p!Cabbage, p!Goat] || any (\sol -> any (== p) sol) sols
+wrongPosition :: Position -> Bool
+wrongPosition p =
+    all (/= p!Farmer) [p!Wolf, p!Goat] || all (/= p!Farmer) [p!Cabbage, p!Goat]
 
 -- шаг переправы с берега на берег с кем-нибудь: какие варианты можно получить
 step :: Position -> [Position]
@@ -44,7 +59,7 @@ type Solution = [Position]
 -- построение нового списка возможных решений из старого
 stepSolution :: [Solution] -> [Solution]
 stepSolution sols =
-    [(newpos:sol) | sol <- sols, newpos <- step (head sol), not $ wrongPosition newpos sols]
+    [(newpos:sol) | sol <- sols, newpos <- step (head sol), not $ wrongPosition newpos]
 
 -- итеративный процесс построения возможных решений,
 -- для поиска среди них того, которое является ответом
@@ -59,7 +74,9 @@ solution = head $ filter ((==goalPosition).head) $ concat $ search
 showSolution = sequence $ map (putStrLn.show.assocs) solution
 
 main = do
-    showSolution
+    if wrongPosition startPosition then sequence $ [print ["wrong position startPosition"]]
+    else if wrongPosition goalPosition then sequence $ [print ["wrong position goalPosition"]]
+    else showSolution
 
 
 
